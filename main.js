ce9848645365cc053b23242578101188077ca462
AdamT2529 <profileytwow1@gmail.com>
2025-09-04 18:41:26 +0000

    Added a if and else code thanks to Mr Hinkle's help.

--- main.js
+++ main.js
@@ -8,6 +8,15 @@ let ti = new TextInterface(app, "Example Text Interface Program");
 ti.output("Welcome to my program!");
 let name = await ti.prompt("What is your name?");
 ti.output("Hello, " + name);
-if name = Adam then
-
-
+if (name === "Adam") {
+  // if it's adam...
+  ti.output("No way it's Adam");
+  
+} 
+let age = await ti. prompt("What is your age?");
+if (age === "16") {
+  ti.output("Wow I am 16 too!");  
+} else {
+  ti.output("That is cool!");
+}
+ti.output("Do you like mango?")
